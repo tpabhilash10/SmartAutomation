0ad82245f53b5c62ddb8c3521a22e838297ae60a
Abhilash T P <tpabhilash10@gmail.com>
2018-10-31 22:59:34 +0530

    comit smart automation project

---
+++ SmartHomeApis/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SmartHomeApis.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SmartHomeApis/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SmartHomeApis.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
